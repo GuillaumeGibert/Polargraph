4af91e6c3c9d223ccde1ec6893fc84272699dfea
Guillaume Gibert <GuillaumeGibert@users.noreply.github.com>
2024-06-14 11:33:09 +0200

    + Test stepper motors connected to the shield

---
+++ arduino/testMotorSteps/testMotorSteps.ino
@@ -0,0 +1,54 @@
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+
+// ----- PARAMS TO TWEAK ----- //
+const int _portBaud = 9600; // baud rate of the serial port
+int _nbSteps = 200; // number of steps to perform
+int _defaultNbStepsStepperMotor = 200; // number of steps / revolution of the stepper motors
+
+// ----- INTERNAL PARAMS ----- //
+// Create the motor shield object with the default I2C address 0x60
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); //write an adress if different
+// Connect a stepper motor with A and B steps per revolution
+// to motor port #1 (M1 and M2)
+Adafruit_StepperMotor *stepperMotorA = AFMS.getStepper(_defaultNbStepsStepperMotor, 1); // A = 200 steps/revolution
+// to motor port #2 (M3 and M4)
+Adafruit_StepperMotor *stepperMotorB = AFMS.getStepper(_defaultNbStepsStepperMotor, 2); // B = 200 steps/revolution
+
+
+void setup() 
+{
+	// initializes the serial port
+	Serial.begin(_portBaud);
+	
+	// initializes the connection with the motor shield 
+	AFMS.begin();  
+}
+
+void loop() 
+{
+	// Motor A complete revolution FORWARD
+  	Serial.println("Motor A --> Forward");
+	for (int l_step=0; l_step < _nbSteps; l_step++)
+		stepperMotorA->onestep(FORWARD,SINGLE); 
+	delay(2000);
+	
+	// Motor B complete revolution BACKWARD
+	Serial.println("Motor B --> Backward");
+	for (int l_step=0; l_step < _nbSteps; l_step++)
+		stepperMotorB->onestep(BACKWARD,SINGLE); 
+	delay(2000);
+	
+	// Motor A complete revolution BACKWARD
+	Serial.println("Motor A --> Backward");
+	for (int l_step=0; l_step < _nbSteps; l_step++)
+		stepperMotorA->onestep(BACKWARD,SINGLE); 
+	delay(2000);
+	
+	// Motor B complete revolution FORWARD
+	Serial.println("Motor B --> Forward");
+	for (int l_step=0; l_step < _nbSteps; l_step++)
+		stepperMotorB->onestep(FORWARD,SINGLE); 
+	delay(2000);
+	
+}
